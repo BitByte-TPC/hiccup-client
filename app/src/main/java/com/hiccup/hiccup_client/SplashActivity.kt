6e79cca2b3ddd6b010cd5dc3c3982acfcbf45a84
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2023-01-22 10:43:53 +0530

    splash screen added

---
+++ app/src/main/java/com/hiccup/hiccup_client/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.hiccup.hiccup_client
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
